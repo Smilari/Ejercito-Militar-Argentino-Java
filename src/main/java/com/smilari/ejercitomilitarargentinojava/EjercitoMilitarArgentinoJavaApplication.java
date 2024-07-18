40058d87e49199a280142b2b8151939e6b91e8b5
“Lucas <lucassmilari2108@gmail.com>
2024-07-18 14:56:39 -0300

    Proyecto vacio creado

---
+++ src/main/java/com/smilari/ejercitomilitarargentinojava/EjercitoMilitarArgentinoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.smilari.ejercitomilitarargentinojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercitoMilitarArgentinoJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercitoMilitarArgentinoJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smilari/ejercitomilitarargentinojava/EjercitoMilitarArgentinoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smilari.ejercitomilitarargentinojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercitoMilitarArgentinoJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
